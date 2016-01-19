5577d56fd212f0dc7ab1a1e1f72b18d601157309
Valentin Haenel <valentin@haenel.co>
2016-01-19 13:42:40 +0100

    shorten

--- aggregate.py
+++ aggregate.py
@@ -2,14 +2,9 @@
 import sys
 results = {}
 for line in sys.stdin.readlines():
-    line = line.strip()
-    splitted = line.split('\t')
-    count, name = splitted
+    count, name = line.strip().split('\t')
     count = int(count)
-    if name not in results:
-        results[name] = count
-    else:
-        results[name] = results[name] + count
+    results[name] = count if name not in results else results[name] + count
 
 for count,name in sorted([(count, name) for name, count in results.items()])[::-1]:
     print "%s\t%s" % (count,name)
